638f53d705adf0abef379849b29bb74373ec2c67
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-28 02:59:55 -0700

    write type anotate function with mapping

---
+++ python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A type annotated simple function"""
+
+from typing import Any, Mapping, TypeVar, Union
+
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+    """A type annotated function"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
