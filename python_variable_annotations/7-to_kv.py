9fd70bbe855af80d1fb2e5eeac9769667b1f54fa
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-28 01:47:19 -0700

    write a type annotated function that returns a tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """A function that returns a tuple"""
-    return tuple([k, float(v * v)])
+    return (k, float(v * v))
