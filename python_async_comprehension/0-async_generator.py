d100bc7c8a2a8c486553674c029e1287b632af66
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-08-28 23:45:44 -0700

    write an asynchronous generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A module with a asynchronous generator"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """An asynchronous generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
