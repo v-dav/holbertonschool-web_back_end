eff300570d3f58fe83502ebb3e904756b588faf9
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-09-05 03:36:05 -0700

    add if name is main to process the code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,18 +5,19 @@ about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
-print("{} logs".format(nginx_collection.count_documents({})))
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print("{} logs".format(nginx_collection.count_documents({})))
 
-methods_count = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
-for method, count in methods_count.items():
-    methods_count[method] = nginx_collection.count_documents(
-        {"method": method})
+    methods_count = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
+    for method, count in methods_count.items():
+        methods_count[method] = nginx_collection.count_documents(
+            {"method": method})
 
-print("Methods:")
-for method, count in methods_count.items():
-    print("\tmethod {}: {}".format(method, count))
+    print("Methods:")
+    for method, count in methods_count.items():
+        print("\tmethod {}: {}".format(method, count))
 
-print("{} status check".format(nginx_collection.count_documents(
-    {"path": "/status"})))
+    print("{} status check".format(nginx_collection.count_documents(
+        {"path": "/status"})))
