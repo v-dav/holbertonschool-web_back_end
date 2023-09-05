47bddb7a17e0b8089d91671e8dfbb6d30df01b91
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-09-05 02:06:26 -0700

    fix update many method

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@ def update_topics(mongo_collection, name, topics):
     """A function that changes all topics of a school document
     based on the name"""
 
-    mongo_collection.update_one(
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
         )
