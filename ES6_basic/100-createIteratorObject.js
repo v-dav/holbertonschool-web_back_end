e8c923147f7b2c7a45349900bf4701ca0e339e00
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-09-13 12:34:46 -0700

    rewrite completely the function to create the iterator

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,38 @@
 export default function createIteratorObject(report) {
-  let allEmployees = [];
-  for (const item of Object.values(report.allEmployees)) {
-    allEmployees = [
-      ...allEmployees,
-      ...item,
-    ];
-  }
-  return allEmployees;
+  // iterable object...
+  const myIterable = {
+    // that has a special method...
+    [Symbol.iterator]() {
+      let departmentIndex = 0;
+      let employeeIndex = 0;
+      // that returns a special object calle d iterator...
+      return {
+        // that has a special method witch returns the next element in the iteration sequence
+        next() {
+          const departmentNames = Object.keys(report.allEmployees);
+          const departmentName = departmentNames[departmentIndex];
+          const department = report.allEmployees[departmentName];
+          if (departmentIndex < Object.keys(report.allEmployees).length) {
+            if (employeeIndex < department.length) {
+              const employee = department[employeeIndex];
+              employeeIndex++;
+              return {
+                value: employee,
+                done: false,
+              };
+            }
+            // go to next department
+            employeeIndex = 0;
+            departmentIndex++;
+            return this.next();
+          }
+          return {
+            value: undefined,
+            done: true,
+          };
+        },
+      };
+    },
+  };
+  return myIterable;
 }
