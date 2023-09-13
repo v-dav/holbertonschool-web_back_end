ebd49b0a1e31afa6b180bf03828f6a1bb34740f4
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-09-13 03:48:52 -0700

    eslint fix

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,17 +1,12 @@
-// a simple functions with constants
-
-// a function with const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// a function
 export function getLast() {
   return ' is okay';
 }
 
-// a function with let
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
