3dc9716d146df492009db0971d999c798c0a5d7a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-03 04:42:06 -0700

    Write an async function named asyncUploadUser that will call these two functions and return an object with a custom format

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const theObject = {
+    photo: null,
+    user: null,
+  };
+  const photo = await uploadPhoto().then((result) => result);
+  const user = await createUser().then((result) => result);
+  if (photo !== undefined && user !== undefined) {
+    theObject.photo = photo;
+    theObject.user = user;
+  }
+  return theObject;
+}
