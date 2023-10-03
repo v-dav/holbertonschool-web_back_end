e65ff4cf06588e377a0fdb0cdb9d5eb226a8352b
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-03 03:40:02 -0700

    factorize the function

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,10 +1,9 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    const value = mathFunction();
-    queue.push(value);
-  } catch (err) {
-    queue.push(`Error: ${err.message}`);
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
