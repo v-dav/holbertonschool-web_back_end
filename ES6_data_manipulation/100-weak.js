522860f1ca6c929c512b5c5cc7bd89b2a0ad24c3
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-10-05 02:27:28 -0700

    modify the condition for the endpoint

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
-  if (count >= 5) {
+  if (count >= 4) {
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(endpoint, count + 1);
