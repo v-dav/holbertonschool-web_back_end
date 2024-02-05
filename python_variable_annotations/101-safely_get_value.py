47c18ae1a62e601d447d6b336987a4d7d6529e37
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-05 12:14:10 +0100

    try to match checker criteria

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """A type annotated simple function"""
 
-from typing import Any, Mapping, TypeVar, Union
+from typing import Any, Mapping, TypeVar, Optional, Union
 
 T = TypeVar("T")
 
 
 def safely_get_value(dct: Mapping,
-                     key: Any, default: Union[T, None]) -> Union[Any, T]:
+                     key: Any, default: Optional[T]) -> Union[Any, T]:
     """A type annotated function"""
     if key in dct:
         return dct[key]
