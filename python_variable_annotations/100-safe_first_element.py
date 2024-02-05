09fddfc4717a456cb4c4e00deba454316b3284df
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-05 12:04:07 +0100

    modify function to match expecting output

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """A type annotated simple function"""
 
-from typing import Any, Sequence, Union
+from typing import Any, Sequence, Optional
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """A function that returns the first element of a list"""
     if lst:
         return lst[0]
