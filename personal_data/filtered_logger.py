1e944dc0ba5bd7ccbafd3e788808e9c42fa69ed7
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-19 11:24:11 +0100

    add from typing import List

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Regex-ing module to obfuscate log messages"""
 
+from typing import List
 import re
 
 
-def filter_datum(fields: list[str],
+def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
                  separator: str) -> str:
