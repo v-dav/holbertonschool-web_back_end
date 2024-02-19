dab4b1af7ae53f25898595d54806c81d81579449
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-19 17:23:33 +0100

    add propagae False attribute

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -44,6 +44,7 @@ def get_logger() -> logging.Logger:
     """A function that creates a logger object"""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
+    logger.propagate = False
     formatter = RedactingFormatter(fields=list(PII_FIELDS))
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
