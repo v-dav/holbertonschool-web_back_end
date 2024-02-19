6e34ca98b646c39c83e7f2aaf51a3b70fe8cbc30
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-19 16:45:46 +0100

    write log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Regex-ing module to obfuscate log messages"""
 
-from typing import List
+from typing import List, Tuple
+import logging
 import re
 
 
@@ -15,3 +16,20 @@ def filter_datum(fields: List[str],
                          f'{field}={redaction}{separator}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
