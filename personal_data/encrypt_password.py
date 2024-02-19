6383ce455c78f1d596bb5d79166f7bc0538c2424
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-19 19:23:10 +0100

    write function that checks that a password matches the previous hashed password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,3 +7,10 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """A function that hashes the password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """A function that checks if pwd is valid"""
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
