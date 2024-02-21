1e92fb4e20a667d863b8148f0012934ce4dc66b8
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-21 04:58:42 -0800

    write extract_base64_authorization_header method

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,14 @@ class BasicAuth(Auth):
     """
     A class to manage Basic HTTP authentication
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header for a Basic
+        Authentication
+        """
+        if (authorization_header is None or
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith("Basic ")):
+            return None
+        return authorization_header.split(" ")[1]
