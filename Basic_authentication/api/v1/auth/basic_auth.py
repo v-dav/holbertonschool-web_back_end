dd987d56f2a740dace732744b2e0974aaf0cd378
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-21 09:06:18 -0800

    Add the method def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'): in the class BasicAuth that returns the User instance based on his email and password.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """A module for Basic authentication"""
 
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,20 @@ class BasicAuth(Auth):
                 ':' not in decoded_base64_authorization_header):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ Returns the User instance based on his email and password
+        """
+        if (user_email is None or not isinstance(user_email, str) or
+                user_pwd is None or not isinstance(user_pwd, str)):
+            return None
+
+        users = User.search({"email": user_email})
+        if users == [] or users is None:
+            return None
+
+        user = users[0]
+        if user.is_valid_password(user_pwd):
+            return user
+        return None
