97171f017e29491d6b82c5ca69a6e9c1d5527cbb
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-21 03:42:54 -0800

    fix pycodestyle requierements

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -27,6 +27,7 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden error handler
