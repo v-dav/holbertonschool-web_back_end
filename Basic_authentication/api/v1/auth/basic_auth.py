8ca6dbc3aa3ac9df5db431077152c57aa950732d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-21 04:42:31 -0800

    create class basic auth extension of auth class

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A module for Basic authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    A class to manage Basic HTTP authentication
+    """
+    pass
