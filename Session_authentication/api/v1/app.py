eb846e68f06321666d75eb7237b1e20cc59a97a2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-22 08:08:07 -0800

    add current_user ofr the request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -52,11 +52,14 @@ def before_request():
                      '/api/v1/unauthorized/',
                      '/api/v1/forbidden/',
                      "/api/v1/stat*"]
+
         if auth.require_auth(request.path, path_list):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
+
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
