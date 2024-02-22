dc904456e058346676b524d75fc7d5bef7b82ec4
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-22 08:32:29 -0800

    Create a class attribute user_id_by_session_id initialized by an empty dictionary

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,18 @@
 """ A module with Session Class authentication"""
 
 from api.v1.auth.basic_auth import BasicAuth
+import uuid
 
 
 class SessionAuth(BasicAuth):
     """A Session Class class for authentication"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """An instance method for creating sessionID"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
