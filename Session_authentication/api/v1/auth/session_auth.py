c80e5140b86d30c3b3f3975e6047fb156205fcc2
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-22 08:20:29 -0800

    create empty session class

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A module with Session Class authentication"""
+
+from api.v1.auth.basic_auth import BasicAuth
+
+
+class SessionAuth(BasicAuth):
+    """A Session Class class for authentication"""
+    pass
