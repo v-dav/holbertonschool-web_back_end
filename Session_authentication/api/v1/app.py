076e764401a35f379e64b1b55b6a4141f95a0d69
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-22 09:08:55 -0800

    Update the @app.before_request method in api/v1/app.py

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -58,10 +58,10 @@ def before_request():
                      "/api/v1/auth_session/login/"]
 
         if auth.require_auth(request.path, path_list):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None \
+                    and auth.session_cookie(request) is None:
                 abort(401)
-            if auth.session_cookie(request) is None:
-                abort(401)
+
             if auth.current_user(request) is None:
                 abort(403)
 
