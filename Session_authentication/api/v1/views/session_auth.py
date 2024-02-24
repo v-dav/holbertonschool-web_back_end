f740a565bc7a26ae8c78207f524972e84459d812
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-24 03:10:07 -0800

    fix api route

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -37,7 +37,7 @@ def session_auth():
     return json_user
 
 
-@app_views.route('/api/v1/auth_session/logout',
+@app_views.route('auth_session/logout',
                  methods=['DELETE'], strict_slashes=False)
 def logout():
     from api.v1.app import auth
