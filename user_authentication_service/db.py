d1f4ff95bdbde9805138e4c1886ce70a8d4b3df1
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-26 08:37:33 -0800

    find user from db

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -36,4 +37,14 @@ class DB:
         self._session.add(user)
         self._session.commit()
 
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ takes in arbitrary keyword arguments and returns
+        the first row found in the users table as filtered by the method’s input arguments."""
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
         return user
