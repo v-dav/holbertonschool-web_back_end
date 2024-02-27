449c07053e6a32535d188527f0cfff0f9a730614
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-27 01:06:55 -0800

    create a function to hash the password in bcrypt

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""The Authentication Module"""
+
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """A function that hashes the password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
