c8fea52b3b524affa4bbbf40b431474811be2fd3
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-28 01:21:52 -0800

    write method for credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -29,3 +29,13 @@ class Auth:
             hashed_pwd = _hash_password(password)
             user = self._db.add_user(email, hashed_pwd)
             return user
+
+    def valid_login(self, email: str, password: bytes) -> bool:
+        """ Credentials validation method"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            return False
+        except NoResultFound:
+            return False
