8cb50b9f0420fd53be3e2f78ed80419d461f223c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-28 01:06:46 -0800

    add register user route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,14 +2,28 @@
 """ Basic flask app module"""
 
 
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def basic():
     """ Basic app route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """End-point to register a user"""
+    try:
+        user = AUTH.register_user(request.form["email"],
+                                  request.form["password"])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
