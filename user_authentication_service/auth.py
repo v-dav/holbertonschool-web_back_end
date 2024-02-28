dd361e9e2d79d53ca85797df8883234d43924130
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-28 07:08:08 -0800

    wrtie a method to create and sotre the session id in the database

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -45,3 +45,13 @@ class Auth:
             return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Method to create the session ID"""
+        try:
+            user = self._db.find_user_by(email=email)
+            user_uuid = _generate_uuid()
+            self._db.update_user(user.id, session_id=user_uuid)
+            return user_uuid
+        except Exception:
+            return None
