ff048d1ee30be012244c405890416cbd507504b7
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-29 09:58:57 -0800

    create generate pwd rest token for auth class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -69,3 +69,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Method to destroy the session"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate pwd reset token"""
+        user = self._db.find_user_by(email=email)
+        if user is not None:
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        else:
+            raise ValueError
