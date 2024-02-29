88112b169c492082ce5ef064db6154b28593a5d7
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-29 10:05:26 -0800

    slightly modify the structure

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -71,11 +71,11 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
-        """Generate pwd reset token"""
-        user = self._db.find_user_by(email=email)
-        if user is not None:
-            reset_token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
-        else:
+        """Generate pwd reset token for app"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
             raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
