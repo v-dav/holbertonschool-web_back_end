95d3b301a96d762e18202042ea66d2eecaa80000
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-29 09:50:40 -0800

    create get user profile route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -53,5 +53,19 @@ def logout():
         abort(403)
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """Get the user profile"""
+    session_id = request.cookies.get("session_id")
+    if session_id is not None:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user is not None:
+            return jsonify({"email": user.email}), 200
+        else:
+            abort(403)
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
