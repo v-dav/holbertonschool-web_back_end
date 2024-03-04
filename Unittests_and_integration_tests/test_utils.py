6e8a122eb095ee15d0705086714b5254d1e1bebc
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-04 07:46:59 -0800

    write a test for a function uzing parameterize module

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Test for utils module
+"""
+
+from parameterized import parameterized
+from utils import access_nested_map
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests for access nested map method"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected) -> None:
+        """Tests the same thing"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
