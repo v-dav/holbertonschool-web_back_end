e3a849d20bf860c6496a13d73ec9946056ca2870
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-06 07:23:26 -0800

    add app run with host and port parameters

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -12,3 +12,7 @@ app = Flask(__name__)
 def welcome():
     """The welcome page"""
     return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
