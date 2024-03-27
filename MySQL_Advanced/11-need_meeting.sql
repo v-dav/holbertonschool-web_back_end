e688b13d043caa0422c5142f1c962483a3cdc019
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-27 06:49:35 -0700

    fix the strict inferiority operator

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -5,4 +5,4 @@
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE score < 80 AND last_meeting is NULL OR last_meeting > DATE_SUB(NOW(), INTERVAL 1 MONTH);
+WHERE score < 80 AND last_meeting is NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
