d9ab8f6351239500b016e4ed495a429c89e380ea
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-03-27 07:51:51 -0700

    factorize the procedure

--- MySQL_Advanced/100-average_weighted_score.sql
+++ MySQL_Advanced/100-average_weighted_score.sql
@@ -5,24 +5,13 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE weighted_avg_score INT;
-	DECLARE sum_weights INT;
-	DECLARE score_x_weighing_factors_sum INT;
-
-	SELECT SUM(weight) INTO sum_weights
-	FROM corrections AS p
-	LEFT JOIN projects
-	ON p.project_id = projects.id
-	WHERE p.user_id = user_id;
-
-	SELECT SUM(score * weight) INTO score_x_weighing_factors_sum
-	FROM corrections AS p
-	LEFT JOIN projects
-	ON p.project_id = projects.id
-	WHERE p.user_id = user_id;
-
-	SET weighted_avg_score = score_x_weighing_factors_sum / sum_weights;
-	UPDATE users SET average_score = weighted_avg_score WHERE id = user_id;
+	UPDATE users
+	SET average_score = (SELECT SUM(
+			(SELECT weight FROM projects WHERE corrections.project_id = id) * score) /
+			(SELECT sum(weight) FROM projects) 
+			FROM corrections 
+			WHERE corrections.user_id = user_id
+			)
+	WHERE id = user_id;
 END $$
 DELIMITER ;
-
