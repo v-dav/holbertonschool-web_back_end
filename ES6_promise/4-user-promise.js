45ebb95e14d6dc783f54a0abd5638c2de8dcae57
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-01 12:10:26 +0200

    write shorter code

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,9 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    resolve({
-      firstName,
-      lastName,
-    });
-    reject();
-  });
+  return Promise.resolve({
+		firstName,
+		lastName,
+	})
 }
