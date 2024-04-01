c5aa2f113d1453a96ee33a1ee94aeaa10b9c315d
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-01 12:13:01 +0200

    get back to the previous version

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,6 +1,9 @@
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-		firstName,
-		lastName,
-	})
+  return new Promise((resolve, reject) => {
+    resolve({
+      firstName,
+      lastName,
+    });
+    reject();
+  });
 }
