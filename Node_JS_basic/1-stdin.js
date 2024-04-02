453c7eb30c9cfceea79cde018a85a2e0ae12e63c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-02 15:29:24 +0200

    add if name is not null condition

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
+  if (name != null) process.stdout.write(`Your name is: ${name}`);
 });
 
-process.on('exit', () => {
+process.on('end', () => {
   console.log('This important software is now closing');
 });
