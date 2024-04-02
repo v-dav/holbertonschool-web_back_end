f55ff5179d051d532b058a0e5e0b5654589ec4b3
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-02 15:27:03 +0200

    using process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
