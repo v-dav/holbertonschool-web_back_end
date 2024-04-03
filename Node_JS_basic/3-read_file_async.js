16f69002f60226489837c0256392cd8f0563a3c7
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-03 09:31:56 +0200

    add detailed comments and transfor to async function with then catch syntax

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,28 +1,54 @@
-const fs = require('fs');
+// import a module needed to work with files
+const fs = require('fs').promises;
 
 async function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) throw new Error('Cannot load the database');
-    const dataArray = data.split('\n').slice(1).filter((line) => line !== '');
-    console.log(`Number of students: ${dataArray.length}`);
+  // get the file data asynchronously
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      // convert data to string
+      const stringData = data.toString();
 
-    const namesbyField = {};
-    dataArray.forEach((student) => {
-      const studentArray = student.split(',');
-      const name = studentArray[0];
-      const field = studentArray[3];
+      // convert data to array and slice the first line containing col descriptions
+      const arrayData = stringData.split('\n').slice(1);
 
-      if (!namesbyField[field]) namesbyField[field] = [];
-      namesbyField[field].push(name);
+      // remove empty lines
+      const filteredArrayData = arrayData.filter((line) => line !== '');
+
+      // print the first message and count students
+      console.log(`Number of students: ${filteredArrayData.length}`);
+
+      // creates an object to store names by field
+      const namesByField = {};
+
+      // for each line, fill the namesByField object
+      filteredArrayData.forEach((line) => {
+        // split each string into an array
+        const parts = line.split(',');
+        // ge the first name and the field
+        const firstName = parts[0];
+        const field = parts[3];
+
+        // check if the field already exists and if not create an empty array as value
+        if (!namesByField[field]) {
+          namesByField[field] = [];
+        }
+
+        // push the name associated with the field into the array
+        namesByField[field].push(firstName);
+      });
+
+      // print the customized message
+      const fields = Object.keys(namesByField);
+      for (const field of fields) {
+        const names = namesByField[field];
+        const count = names.length;
+        const list = names.join(', ');
+        console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
     });
-    const fields = Object.keys(namesbyField);
-    for (const field of fields) {
-      const names = namesbyField[field];
-      const count = names.length;
-      console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
-    }
-  });
 }
 
-countStudents('database.csv');
 module.exports = countStudents;
