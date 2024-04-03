1b4cbd8329c5160b7c4eabd648051569c7244430
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-03 09:40:56 +0200

    Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = 1245;
+const hostname = '127.0.0.1';
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname);
