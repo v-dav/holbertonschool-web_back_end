2efa8c57ae78fae234dc9595f3e75d3ddded1698
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-11 11:25:43 +0200

    write a basic Redis-based queuing system where we have a process to generate job and a second one to process it.

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,16 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    process.exit();
+  }
+});
