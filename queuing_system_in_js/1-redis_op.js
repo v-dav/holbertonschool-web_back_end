996616a4f9c8ccbf52925a483fbb7a1b54d4f624
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-04-11 09:25:06 +0200

    set and get with redis synch functions

--- queuing_system_in_js/1-redis_op.js
+++ queuing_system_in_js/1-redis_op.js
@@ -1,6 +1,20 @@
-import { createClient } from 'redis';
+import { createClient, print } from 'redis';
 
 const client = createClient();
 
 client.on('error', (err) => console.log('Redis client not connected to the server:', err));
 client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
